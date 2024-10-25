8a5d7b7bd2e021d2927232db56263ec8b995b4d5
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2024-10-25 18:58:35 +0530

    Pricing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <FeatureSection />
+      <Workflow />
+      <Pricing />
+    </>
+  );
+}
+
+export default App;
