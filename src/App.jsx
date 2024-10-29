e6e2aa8d81c4957cc183f5d92387880afd1e498b
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2024-10-29 17:16:31 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
 
-function App() {
+const App = () => {
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <FeatureSection />
-      <Workflow />
-      <Pricing />
-      <Testimonials />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
     </>
   );
-}
+};
 
 export default App;
